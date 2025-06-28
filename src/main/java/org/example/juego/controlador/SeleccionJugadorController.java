b4694b5e558709167504573aa114a8e3a7de4e5e
japadron-24 <japadron.24@est.ucab.edu.ve>
2025-06-28 16:30:26 -0400

    Añadir funcionalidad para expandir información del jugador en el tablero

--- src/main/java/org/example/juego/controlador/SeleccionJugadorController.java
+++ src/main/java/org/example/juego/controlador/SeleccionJugadorController.java
@@ -234,7 +234,7 @@ public class SeleccionJugadorController {
 
         try {
             javafx.fxml.FXMLLoader loader = new javafx.fxml.FXMLLoader(
-                    getClass().getResource("/org/example/juego/tableroView.fxml"));
+                    getClass().getResource("/org/example/juego/TableroView.fxml"));
             Parent root = loader.load();
             Stage tableroStage = new Stage();
 
